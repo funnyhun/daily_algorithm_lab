c7b499fc328eafa6110f55941caeb4d523ba8ee2
funnyhun <funnyhun0222@gmail.com>
2025-08-20 02:20:34 +0900

    modify : BOJ_11654_아스키코드

--- BaekJoon/11654_아스키코드/solution.js
+++ BaekJoon/11654_아스키코드/solution.js
@@ -4,7 +4,8 @@ import { test_funcs } from "../../util/test.js";
 const input = local_input("./input.txt");
 
 // 주어진 문자열을 아스키 코드로 변환하여 출력하는 문제
-// js에는 String 객체 내부에 해당 기능을 하는 메소드가 존재한다.
+// 1. js에는 String 객체 내부에 해당 기능을 하는 메소드가 존재한다.
+// 2. js에는 문자열이 아닌 단일 문자도 String 객체로 생성된다.
 function solve_1(input) {
   return input[0].charCodeAt(input);
 }
