92281d799581d029e3d594f3dc73ffb38676f3c4
funnyhun <funnyhun0222@gmail.com>
2025-08-24 14:36:36 +0900

    modify : BOJ_1152_단어의 개수

--- BaekJoon/1152_단어의 개수/solution.js
+++ BaekJoon/1152_단어의 개수/solution.js
@@ -5,7 +5,9 @@ const input = local_input("./input.txt");
 
 // 주어진 문자열의 공백을 토대로 단어의 개수를 출력하는 문제
 function solve_1(input) {
-  return input[0].split(" ").length;
+  // 주어진 조건 중, 문자열이 공백으로 시작하거나 끝날 수 있다는 조건이 존재
+  // -> split(" ")으로 분리 시 문자열 내 공백들이 배열 요소로 존재하는 예외 발생 -> filter()를 통해 빈문자열 제거
+  return input[0].split(" ").filter((e) => e !== "").length;
 }
 
 test_funcs([input], [solve_1]);
