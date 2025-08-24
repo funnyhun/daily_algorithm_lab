b4d4375dad2227bcb1213f60aef9fcda31cbee28
funnyhun <funnyhun0222@gmail.com>
2025-08-24 14:29:33 +0900

    feat : BOJ_template

---
+++ BaekJoon/template/solution.js
@@ -0,0 +1,8 @@
+import { local_input } from "../../util/input.js";
+import { test_funcs } from "../../util/test.js";
+
+const input = local_input("./input.txt");
+
+// insert your codes
+
+test_funcs([input], [solve_1]);
